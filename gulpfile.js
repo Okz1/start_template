8449ac63c6847c584fee05d0218768290a3f917b
Oleg Mykhalyna <oleg.muhaluna@gmail.com>
2017-09-06 18:23:31 +0300

    5

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
+// Підключаємо бібліотеки 
 var gulp  			= require('gulp'),
 		sass        = require('gulp-sass'),
-		browserSync = require('browser-sync');
+		browserSync = require('browser-sync'),
+		concat      = require('gulp-concat'), //конкатинація 
+		uglify 			=	require('gulp-uglifyjs'), // зжимаємо js файли
+		cssnano  		=	require('gulp-cssnano'),// мініфікація css
+		rename			=	require('gulp-rename');
 
 //описуємо таск сасс 
 gulp.task('sass',function(){
@@ -8,6 +13,26 @@ gulp.task('sass',function(){
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.reload({stream: true})) //інжектимо нащі стилі
+});
+
+
+//Сюди підключаємо бібліотеки для конкатинації і мініфікації
+gulp.task('scripts', function(){
+	return gulp.src([ // беремо потрібні нам файли для конкатинації
+			'app/libs/jquery/dist/jquery.min.js',
+			'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		])
+	.pipe(concat('libs.min.js')) // мінифіцирований файл з бібліотеами js
+	.pipe(uglify()) // зжимаємо файл
+	.pipe(gulp.dest('app/js')); // вигружаємо файл сюди
+});
+
+//мініфікація і перейменування libs.css
+gulp.task('css-libs',['sass'], function(){
+	return gulp.src('app/css/libs.css') // вибір файлу
+	.pipe(cssnano()) // мініфікація
+	.pipe(rename({suffix: '.min'}))// добавляємо суфікс
+	.pipe(gulp.dest('app/css')); // вибираємо розташування
 });
 
 // описуємо таск browserSync
@@ -21,7 +46,7 @@ gulp.task('browser-sync', function(){
 }); 
 
 //описуємо таск watch який слідкує за зміною в файлах
-gulp.task('watch',['browser-sync', 'sass'], function() { //в квадратних дужках перераховуємо таски, які треба виконати до запуску таску watch, в даному випадку браузер сінк і сасс
+gulp.task('watch',['browser-sync', 'css-libs', 'scripts'], function() { //в квадратних дужках перераховуємо таски, які треба виконати до запуску таску watch, в даному випадку браузер сінк і сасс
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload); // слідкуємо за змінами в html файлі
 	gulp.watch('app/js/**/*.js', browserSync.reload);	 // слідкуємо за змінами в js файлі
