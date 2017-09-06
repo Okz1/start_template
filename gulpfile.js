d930433cfd5297cb997a0c0e94bc4dfa95ad878a
Oleg Mykhalyna <oleg.muhaluna@gmail.com>
2017-09-06 17:16:33 +0300

    4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp  			= require('gulp'),
 		sass        = require('gulp-sass'),
 		browserSync = require('browser-sync');
+
 //описуємо таск сасс 
 gulp.task('sass',function(){
 	return gulp.src('app/sass/**/*.sass')
@@ -9,17 +10,19 @@ gulp.task('sass',function(){
 		.pipe(browserSync.reload({stream: true})) //інжектимо нащі стилі
 });
 
-//описуємо таск watch який слідкує за зміною в файлах
-gulp.task('watch',['browser-sync'], function(){ //в квадратних дужках перераховуємо таски, які треба виконати до запуску таску watch, в даному випадку браузер сінк
-	gulp.watch('app/sass/**/*.sass', ['sass']);
-});
-
 // описуємо таск browserSync
 gulp.task('browser-sync', function(){
 	browserSync({
 		server: {
 			baseDir: 'app' // вибираємо папку'сервер'
-		}
-		notify: false // відключаємо спливаючі вікна в браузері
+		},
+		notify: false  // відключаємо спливаючі вікна в браузері
 	});
 }); 
+
+//описуємо таск watch який слідкує за зміною в файлах
+gulp.task('watch',['browser-sync', 'sass'], function() { //в квадратних дужках перераховуємо таски, які треба виконати до запуску таску watch, в даному випадку браузер сінк і сасс
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload); // слідкуємо за змінами в html файлі
+	gulp.watch('app/js/**/*.js', browserSync.reload);	 // слідкуємо за змінами в js файлі
+});
