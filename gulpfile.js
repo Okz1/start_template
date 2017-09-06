0ed748eb24b3dcbb50d355977b85ba299e1b7b68
Oleg Mykhalyna <oleg.muhaluna@gmail.com>
2017-09-06 19:37:51 +0300

    last

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
-// Підключаємо бібліотеки 
-var gulp  			= require('gulp'),
-		sass        = require('gulp-sass'),
-		browserSync = require('browser-sync'),
-		concat      = require('gulp-concat'), //конкатинація 
-		uglify 			=	require('gulp-uglifyjs'), // зжимаємо js файли
-		cssnano  		=	require('gulp-cssnano'),// мініфікація css
-		rename			=	require('gulp-rename');
+// Підключаємо модулі 
+var gulp  				= require('gulp'),
+		sass        	= require('gulp-sass'),
+		browserSync 	= require('browser-sync'),
+		concat      	= require('gulp-concat'), //конкатинація 
+		uglify 				=	require('gulp-uglifyjs'),// зжимаємо js файли
+		autoprefixer 	= require('gulp-autoprefixer'); // автопрефіксер
+		// cssnano  		=	require('gulp-cssnano'), // мініфікація css хз чи працює нормально
+		// rename			=	require('gulp-rename'),
+		// del					= require('del'), //хз чому не працює
+		// imagemin		= require('gulp-imagemin'),// хз чи працює нормально
+		// pngquant		=	require('imagemin-pngquant');// хз чи працює нормально
 
 //описуємо таск сасс 
-gulp.task('sass',function(){
+gulp.task('sass' , function(){
 	return gulp.src('app/sass/**/*.sass')
 		.pipe(sass())
+		.pipe(autoprefixer(['> 1%', 'ie 8', 'ie 7','last 15 versions'],{ cascade: true}))
 		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({stream: true})) //інжектимо нащі стилі
+		.pipe(browserSync.reload({stream: true})) //інжектимо наші стилі
 });
 
 
@@ -27,13 +32,13 @@ gulp.task('scripts', function(){
 	.pipe(gulp.dest('app/js')); // вигружаємо файл сюди
 });
 
-//мініфікація і перейменування libs.css
-gulp.task('css-libs',['sass'], function(){
-	return gulp.src('app/css/libs.css') // вибір файлу
-	.pipe(cssnano()) // мініфікація
-	.pipe(rename({suffix: '.min'}))// добавляємо суфікс
-	.pipe(gulp.dest('app/css')); // вибираємо розташування
-});
+// //мініфікація і перейменування libs.css
+// gulp.task('css-libs',['sass'], function(){
+// 	return gulp.src('app/css/libs.css') // вибір файлу
+// 	.pipe(cssnano()) // мініфікація
+// 	.pipe(rename({suffix: '.min'}))// добавляємо суфікс
+// 	.pipe(gulp.dest('app/css')); // вибираємо розташування
+// });
 
 // описуємо таск browserSync
 gulp.task('browser-sync', function(){
@@ -45,9 +50,46 @@ gulp.task('browser-sync', function(){
 	});
 }); 
 
+// //очистка папки dist
+// gulp.task('clean', function(){
+// 	return del.sync('dist'); //хз чому не працює
+// });
+
+//обробка фото
+// gulp.task("img", function(){
+// 	return gulp.src('app/img/**/*')	
+// 	.pipe(imagemin({
+// 		interlaced: true,
+// 		progressive: true,
+// 		svgPlugins: [{removeViewBox: false}],
+// 		use: [pngquant()]
+// 	}))
+// 	.pipe(gulp.dest('dist/img'));	
+// });
+
 //описуємо таск watch який слідкує за зміною в файлах
-gulp.task('watch',['browser-sync', 'css-libs', 'scripts'], function() { //в квадратних дужках перераховуємо таски, які треба виконати до запуску таску watch, в даному випадку браузер сінк і сасс
+gulp.task('watch',['browser-sync', 'sass', 'scripts'], function() { //в квадратних дужках перераховуємо таски, які треба виконати до запуску таску watch, в даному випадку браузер сінк і сасс
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload); // слідкуємо за змінами в html файлі
 	gulp.watch('app/js/**/*.js', browserSync.reload);	 // слідкуємо за змінами в js файлі
 });
+
+// //таск для компіляції робочої папки в продакшен
+// gulp.task('build',['clean','sass','scripts'], function(){
+
+// 	var buildCss = gulp.src([
+// 			'app/css/main.css',
+// 			'app/css/libs.min.css',
+
+// 		])
+// 		.pipe(gulp.dest('dist/css')); //переносимо css в dist
+
+// 	var buildFonts = gulp.src('app/fonts/**/*')
+// 		.pipe(gulp.dest('dist/fonts')); //переносимо fonts в dist
+
+// 	var buildJs = gulp.src('app/js/**/*')
+// 		.pipe(gulp.dest('dist/js')); //переносимо js в dist
+
+// 	var buildHtml = gulp.src('app/*.html')
+// 		.pipe(gulp.dest('dist')); //переносимо html в dist
+// });
